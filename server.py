e7da85817824ca03b53f2d58b1c63ea6583f3c7a
zlailari <lailari.zeev@gmail.com>
2016-02-20 17:54:03 -0600

    random search and stuff

--- server.py
+++ server.py
@@ -7,17 +7,17 @@ app = Flask(__name__, static_url_path='')
 def serve():
     return app.send_static_file('map.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        for user in bigLoginObj:
-            if user['username'] == request.form['username'] &&
-               user['password'] == request.form['password']:
-               return redirect(url_for('home'))
-        else:
-            error = 'Invalid Credentials. Please try again.'
-    return redirect('login.html', error=error)
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     error = None
+#     if request.method == 'POST':
+#         for user in bigLoginObj:
+#             if user['username'] == request.form['username'] &&
+#                user['password'] == request.form['password']:
+#                return redirect(url_for('home'))
+#         else:
+#             error = 'Invalid Credentials. Please try again.'
+#     return redirect('login.html', error=error)
 
 
 
