dc91962289fcbc7e4d93a89f1d404a18baf697ee
David Latva <latvad@umich.edu>
2016-02-20 13:48:48 -0500

    decreased delay, output is json

--- genLoc.py
+++ genLoc.py
@@ -1,15 +1,23 @@
 import requests
 import random
 import time
+import json
+
+def attachLocation(summoner):
+    lat = random.randrange(32,40)
+    long = random.randrange(-120,-80)
+    summoner["location"] = (lat, long)
+
+def attachRanking(summoner):
+    pass
 
 summId = 521955
 for x in range(0,9):
     url = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/"+str(summId)+"?api_key=184d689a-f9d9-4f1f-b178-e688e5348e15"
-    response = requests.get(url)
-    lat = random.randrange(32,40)
-    long = random.randrange(-120,-80)
-    summoner = response.json()
-    summoner["location"] = (lat, long)
-    print (summoner)
+    summoner = requests.get(url).json()
+    attachLocation(summoner)
+    attachRanking
+    print (json.dumps(summoner))
     summId+=1
-    time.sleep(2)
+    time.sleep(1.5)
+#no more than 500 req per 10m or 10 per 10s
