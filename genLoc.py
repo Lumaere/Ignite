924392ede391d46f809364d487a2ff958bcc4c0d
David Latva <latvad@umich.edu>
2016-02-20 16:15:38 -0500

    summoner from leaderboards

--- genLoc.py
+++ genLoc.py
@@ -8,16 +8,18 @@ def attachLocation(summoner):
     long = random.randrange(-120,-80)
     summoner["location"] = (lat, long)
 
-def attachRanking(summoner):
-    pass
+#def attachRanking(summoner, summId):
+    #entryUrl = "https://na.api.pvp.net/api/lol/na/v2.5/league/by-summoner/"+str(summId)+"/entry?api_key=184d689a-f9d9-4f1f-b178-e688e5348e15"
+    #summoner["entry"] = requests.get(entryUrl).json()
 
-summId = 521955
-for x in range(0,9):
-    url = "https://na.api.pvp.net/api/lol/na/v1.4/summoner/"+str(summId)+"?api_key=184d689a-f9d9-4f1f-b178-e688e5348e15"
-    summoner = requests.get(url).json()
-    attachLocation(summoner)
-    attachRanking
-    print (json.dumps(summoner))
-    summId+=1
-    time.sleep(1.5)
+pageNum = 1
+for x in range(0,2):
+    url = "http://www.lolking.net/leaderboards/285ca6feadf6357d815b0d05c66caf11/na/"+str(pageNum)+".json"
+    page = requests.get(url).json()["data"]
+    #print (json.dumps(page))
+    for summoner in page:
+        attachLocation(summoner)
+        print(json.dumps(summoner))
+    pageNum+=1
+    time.sleep(1)
 #no more than 500 req per 10m or 10 per 10s
